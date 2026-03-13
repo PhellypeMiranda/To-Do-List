b6032146f682655be44e52dbde35cd7e715344d2
Phellype <phellypewandel@protonmail.com>
2026-03-13 14:32:13 -0300

    Colocada a função de ler a lista

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from unittest import case
+
+
 def main():
-#Menu principal, o usuário vai escolher qual caminho seguir.
+    #Variável que vai guardar a lista de coisas para fazer
+    lista = []
+
+    #Menu principal, o usuário vai escolher qual caminho seguir.
     while True:
         try:
             escolha = int(input("O que gostaria de fazer?\n"
@@ -9,11 +15,24 @@ def main():
                             "4 - Apagar lista\n"
                             "5 - Sair\n"
                             "Digite um valor: "))
-            break
+
+            match escolha:
+                case 1:
+                    if len(lista) == 0:
+                        input("Lista vazia!\n ")
+                    else:
+                        print(f"\nTO DO!")
+                        for i in lista:
+                            print(f"- {i}")
+                        input("Aperte qualquer tecla para continuar")
+
+        #Except para caso o usuário digitar uma letra
         except ValueError:
             input("\nValor inválido, por favor digite novamente!\n ")
-
+        #Except para erros não previstos
         except:
             input("\nAlgo deu errado, por favor tente novamente!\n ")
+
+
 
 main()
